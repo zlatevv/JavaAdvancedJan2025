0b2dfb23242232f2f99beabc54e7b4ccfce6065c
ExamPrep <cvetazlatev30@gmail.com>
2025-02-05 15:30:54 +0200

    iterators and comparators

---
+++ src/Book/BookComparator.java
@@ -0,0 +1,15 @@
+package Book;
+
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+
+    @Override
+    public int compare(Book book1, Book book2) {
+        int result = book1.getTitle().compareTo(book2.getTitle());
+        if (result == 0){
+            result = Integer.compare(book1.getYear(),book2.getYear());
+        }
+        return result;
+    }
+}
